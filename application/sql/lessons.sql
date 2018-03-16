93c8e74e64c61f23932079d3a7f612bf3b43e9ea
Szymon <szymonkomander@gmail.com>
2018-03-16 14:47:33 +0100

    Database for login

--- application/sql/lessons.sql
+++ application/sql/lessons.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.7.7
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Czas generowania: 08 Mar 2018, 14:54
--- Wersja serwera: 5.7.19
--- Wersja PHP: 7.1.9
+-- Host: 127.0.0.1
+-- Czas generowania: 16 Mar 2018, 14:45
+-- Wersja serwera: 10.1.30-MariaDB
+-- Wersja PHP: 7.2.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Baza danych: `projekt-pon`
+-- Baza danych: `lessons`
 --
 
 -- --------------------------------------------------------
@@ -28,23 +28,40 @@ SET time_zone = "+00:00";
 -- Struktura tabeli dla tabeli `days`
 --
 
-DROP TABLE IF EXISTS `days`;
-CREATE TABLE IF NOT EXISTS `days` (
+CREATE TABLE `days` (
   `id` int(11) NOT NULL,
-  `name` varchar(64) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `name` varchar(64) CHARACTER SET utf8 NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
 
 --
 -- Zrzut danych tabeli `days`
 --
 
 INSERT INTO `days` (`id`, `name`) VALUES
-(1, 'Poniedzialek'),
-(2, 'wtorek'),
-(3, 'sroda'),
-(4, 'czwartek'),
-(5, 'piatek');
+(1, 'Poniedziałek'),
+(2, 'Wtorek'),
+(3, 'Środa'),
+(4, 'Czwartek'),
+(5, 'Piątek');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `groups`
+--
+
+CREATE TABLE `groups` (
+  `id` int(11) NOT NULL,
+  `name` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Zrzut danych tabeli `groups`
+--
+
+INSERT INTO `groups` (`id`, `name`) VALUES
+(1, 'Grupa W1'),
+(2, 'Grupa W2');
 
 -- --------------------------------------------------------
 
@@ -52,12 +69,10 @@ INSERT INTO `days` (`id`, `name`) VALUES
 -- Struktura tabeli dla tabeli `lessons`
 --
 
-DROP TABLE IF EXISTS `lessons`;
-CREATE TABLE IF NOT EXISTS `lessons` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(64) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `lessons` (
+  `id` int(11) NOT NULL,
+  `name` varchar(64) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
 
 --
 -- Zrzut danych tabeli `lessons`
@@ -79,17 +94,32 @@ INSERT INTO `lessons` (`id`, `name`) VALUES
 -- Struktura tabeli dla tabeli `plan`
 --
 
-DROP TABLE IF EXISTS `plan`;
-CREATE TABLE IF NOT EXISTS `plan` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `lesson` varchar(64) CHARACTER SET utf8mb4 NOT NULL,
-  `day` varchar(64) CHARACTER SET utf8mb4 NOT NULL,
-  `type` varchar(64) CHARACTER SET utf8mb4 NOT NULL,
+CREATE TABLE `plan` (
+  `id` int(11) NOT NULL,
+  `lesson` varchar(64) NOT NULL,
+  `day` varchar(64) NOT NULL,
+  `type` varchar(64) NOT NULL,
   `start` time NOT NULL,
   `end` time NOT NULL,
-  `teacher_name` varchar(64) CHARACTER SET utf8mb4 NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+  `teacher_name` varchar(64) NOT NULL,
+  `group_number` varchar(64) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+--
+-- Zrzut danych tabeli `plan`
+--
+
+INSERT INTO `plan` (`id`, `lesson`, `day`, `type`, `start`, `end`, `teacher_name`, `group_number`) VALUES
+(1, 'Narz?dzia sztucznej inteligencji', 'Wtorek', 'Projekt', '00:00:00', '00:00:00', 'dasda', 'Grupa W2'),
+(2, 'Techniki internetowe', 'Poniedzia?ek', 'Projekt', '00:00:00', '00:00:00', 'yhhyh', 'Grupa W2'),
+(3, 'Bazy danych II', 'Poniedzia?ek', '?wiczenia', '00:00:00', '00:00:00', 'dsadas', 'Grupa W2'),
+(4, 'Techniki internetowe', 'Czwartek', 'Projekt', '11:10:00', '12:30:00', 'ghfhfghf', 'Grupa W1'),
+(5, 'Techniki internetowe', 'Wtorek', 'Laboratorium', '13:03:00', '14:04:00', 'Gola', 'Grupa W2'),
+(6, 'Narz?dzia sztucznej inteligencji', '?roda', 'Laboratorium', '02:03:00', '04:05:00', 'Kowalski', 'Grupa W2'),
+(7, 'Techniki internetowe', 'Wtorek', 'Laboratorium', '02:03:00', '04:05:00', 'Nowak', 'Grupa W1'),
+(8, 'Praca przej?ciowa', 'Czwartek', 'Laboratorium', '01:02:00', '03:04:00', 'Nowak', 'Grupa W1'),
+(11, 'Techniki internetowe', 'Czwartek', 'Laboratorium', '05:06:00', '07:08:00', 'Fedek', 'Grupa W1'),
+(12, 'Zarz?dzanie projektami informatycznymi', 'Wtorek', 'Laboratorium', '03:05:00', '06:07:00', 'Assda', 'Grupa W2');
 
 -- --------------------------------------------------------
 
@@ -97,12 +127,10 @@ CREATE TABLE IF NOT EXISTS `plan` (
 -- Struktura tabeli dla tabeli `type`
 --
 
-DROP TABLE IF EXISTS `type`;
-CREATE TABLE IF NOT EXISTS `type` (
+CREATE TABLE `type` (
   `id` int(11) NOT NULL,
-  `Name` varchar(64) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `Name` varchar(64) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
 
 --
 -- Zrzut danych tabeli `type`
@@ -110,9 +138,98 @@ CREATE TABLE IF NOT EXISTS `type` (
 
 INSERT INTO `type` (`id`, `Name`) VALUES
 (1, 'Laboratorium'),
-(2, 'Wyklad'),
-(3, 'Cwiczenia'),
+(2, 'Wykład'),
+(3, 'Ćwiczenia'),
 (4, 'Projekt');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `login` varchar(64) NOT NULL,
+  `pass` varchar(64) NOT NULL,
+  `group_number` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Zrzut danych tabeli `users`
+--
+
+INSERT INTO `users` (`id`, `login`, `pass`, `group_number`) VALUES
+(1, 'krzysiek', 'szczupak', 'Grupa W2'),
+(2, 'szymon', 'okon', 'Grupa W1');
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `days`
+--
+ALTER TABLE `days`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksy dla tabeli `groups`
+--
+ALTER TABLE `groups`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksy dla tabeli `lessons`
+--
+ALTER TABLE `lessons`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksy dla tabeli `plan`
+--
+ALTER TABLE `plan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksy dla tabeli `type`
+--
+ALTER TABLE `type`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksy dla tabeli `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `groups`
+--
+ALTER TABLE `groups`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT dla tabeli `lessons`
+--
+ALTER TABLE `lessons`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT dla tabeli `plan`
+--
+ALTER TABLE `plan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT dla tabeli `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
