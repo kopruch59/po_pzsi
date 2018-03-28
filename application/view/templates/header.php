fa84b4d8dcf5bbe25ff821bbf1d854c055cd4143
Szymon <szymonkomander@gmail.com>
2018-03-28 09:55:29 +0200

    Improved login look.

--- application/view/templates/header.php
+++ application/view/templates/header.php
@@ -7,7 +7,7 @@
    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous" />
    <link rel="stylesheet" href="../public/css/style.css" type="text/css" />
     </head>
-    <body>
+    <body class="text-center">
         <div id="menu">
             <a href="../home/index" class="neutrallink"><div class="option">home</div></a>
             <a href="../home/login" class="neutrallink"><div class="option">home/login</div></a>
