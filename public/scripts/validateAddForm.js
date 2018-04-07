dee96a77f0d6585c8330d20cce0e14789c0fae0b
Szymon <szymonkomander@gmail.com>
2018-04-07 17:59:06 +0200

    Validation of adding form & saving form values.

---
+++ public/scripts/validateAddForm.js
@@ -0,0 +1,133 @@
+// SELECTING ALL TEXT ELEMENTS
+var subject_name = document.forms['addForm']['subject_name'];
+var day = document.forms['addForm']['day'];
+var type = document.forms['addForm']['type'];
+var start_time = document.forms['addForm']['start_time'];
+var end_time = document.forms['addForm']['end_time'];
+var teacher_name = document.forms['addForm']['teacher_name'];
+var group = document.forms['addForm']['group'];
+// SELECTING ALL ERROR DISPLAY ELEMENTS
+var subject_name_error = document.getElementById('subject_name_error');
+var day_error = document.getElementById('day_error');
+var type_error = document.getElementById('type_error');
+var start_time_error = document.getElementById('start_time_error');
+var end_time_error = document.getElementById('end_time_error');
+var teacher_name_error = document.getElementById('teacher_name_error');
+var group_error = document.getElementById('group_error');
+// SETTING ALL EVENT LISTENERS
+subject_name.addEventListener('blur', subjectNameVerify, true);
+day.addEventListener('blur', dayVerify, true);
+type.addEventListener('blur', typeVerify, true);
+start_time.addEventListener('blur', startTimeVerify, true);
+end_time.addEventListener('blur', endTimeVerify, true);
+teacher_name.addEventListener('blur', teacherNameVerify, true);
+group.addEventListener('blur', groupVerify, true);
+// validation function
+function Validate() {
+    if (subject_name.value == "") {
+        subject_name.style.border = "1px solid red";
+        document.getElementById('lessons').style.color = "red";
+        subject_name_error.textContent = "Pole wybierz przedmiot jest wymagane!";
+        subject_name.focus();
+        return false;
+    }
+    if (day.value == "") {
+        day.style.border = "1px solid red";
+        document.getElementById('day').style.color = "red";
+        day_error.textContent = "Pole wybierz dzień jest wymagane!";
+        day.focus();
+        return false;
+    }
+    if (type.value == "") {
+        type.style.border = "1px solid red";
+        document.getElementById('type').style.color = "red";
+        type_error.textContent = "Pole wybierz typ jest wymagane!";
+        type.focus();
+        return false;
+    }
+    if (start_time.value == "") {
+        start_time.style.border = "1px solid red";
+        document.getElementById('start_time').style.color = "red";
+        start_time_error.textContent = "Pole godzina rozpoczęcia jest wymagana!";
+        start_time.focus();
+        return false;
+    }
+    if (end_time.value == "") {
+        end_time.style.border = "1px solid red";
+        document.getElementById('end_time').style.color = "red";
+        end_time_error.textContent = "Pole godzina zakończenia jest wymagana!";
+        end_time.focus();
+        return false;
+    }
+    if (teacher_name.value == "") {
+        teacher_name.style.border = "1px solid red";
+        document.getElementById('teacher_name').style.color = "red";
+        teacher_name_error.textContent = "Pole nazwisko prowadzącego jest wymagane!";
+        teacher_name.focus();
+        return false;
+    }
+    if (group.value == "") {
+        group.style.border = "1px solid red";
+        document.getElementById('group').style.color = "red";
+        group_error.textContent = "Pole grupa jest wymagane!";
+        group.focus();
+        return false;
+    }
+}
+// event handler functions
+function subjectNameVerify() {
+    if (subject_name.value != "") {
+        subject_name.style.border = "1px solid #5e6e66";
+        document.getElementById('lessons').style.color = "#5e6e66";
+        subject_name_error.innerHTML = "";
+        return true;
+    }
+}
+function dayVerify() {
+    if (day.value != "") {
+        day.style.border = "1px solid #5e6e66";
+        document.getElementById('day').style.color = "#5e6e66";
+        day_error.innerHTML = "";
+        return true;
+    }
+}
+function typeVerify() {
+    if (type.value != "") {
+        type.style.border = "1px solid #5e6e66";
+        document.getElementById('type').style.color = "#5e6e66";
+        type_error.innerHTML = "";
+        return true;
+    }
+}
+function startTimeVerify() {
+    if (start_time.value != "") {
+        start_time.style.border = "1px solid #5e6e66";
+        document.getElementById('start_time').style.color = "#5e6e66";
+        start_time_error.innerHTML = "";
+        return true;
+    }
+}
+function endTimeVerify() {
+    if (end_time.value != "") {
+        end_time.style.border = "1px solid #5e6e66";
+        document.getElementById('end_time').style.color = "#5e6e66";
+        end_time_error.innerHTML = "";
+        return true;
+    }
+}
+function teacherNameVerify() {
+    if (teacher_name.value != "") {
+        teacher_name.style.border = "1px solid #5e6e66";
+        document.getElementById('teacher_name').style.color = "#5e6e66";
+        teacher_name_error.innerHTML = "";
+        return true;
+    }
+}
+function groupVerify() {
+    if (group.value != "") {
+        group.style.border = "1px solid #5e6e66";
+        document.getElementById('group').style.color = "#5e6e66";
+        group_error.innerHTML = "";
+        return true;
+    }
+}
